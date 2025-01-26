cd72394a400c95d5fc194388a885a4db7dfc1ff4
Prokop <prokop.schield@protonmail.com>
2025-01-26 18:49:20 +0100

    Release: v0.1.0-6

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use ps_alloc::{AllocationError, DeallocationError};
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq, Eq, Clone)]
 pub enum BufferError {
     #[error(transparent)]
     AllocationError(#[from] AllocationError),
