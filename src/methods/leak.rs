e999b5c1fdc65ad1c77a595e361f59e562b7d09b
Prokop <prokop.schield@protonmail.com>
2025-04-06 09:13:03 +0200

    docs(src/methods/leak.rs): document the requirement to use `ps_alloc::free`

--- src/methods/leak.rs
+++ src/methods/leak.rs
@@ -3,7 +3,7 @@ use std::slice::from_raw_parts_mut;
 use crate::Buffer;
 
 impl Buffer {
-    /// Leaks this Buffer's memory. Caller shall ensure the leaked memory is later freed with correct alignment.
+    /// Leaks this Buffer's memory. Caller shall ensure the leaked memory is later freed with [`ps_alloc::free`].
     #[must_use]
     pub fn leak(self) -> &'static mut [u8] {
         let len = self.len();
