21b14a249fd70f824c929875cdbf1e852e288470
Prokop <prokop.schield@protonmail.com>
2025-04-14 15:31:39 +0200

    fix: return empty slice if length empty

--- src/implementations/deref.rs
+++ src/implementations/deref.rs
@@ -6,6 +6,10 @@ impl Deref for Buffer {
     type Target = [u8];
 
     fn deref(&self) -> &Self::Target {
-        unsafe { std::slice::from_raw_parts(self.as_ptr(), self.len()) }
+        if self.is_empty() {
+            &[]
+        } else {
+            unsafe { std::slice::from_raw_parts(self.as_ptr(), self.len()) }
+        }
     }
 }
--- src/implementations/deref_mut.rs
+++ src/implementations/deref_mut.rs
@@ -4,6 +4,10 @@ use crate::Buffer;
 
 impl DerefMut for Buffer {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { std::slice::from_raw_parts_mut(self.as_mut_ptr(), self.len()) }
+        if self.is_empty() {
+            &mut []
+        } else {
+            unsafe { std::slice::from_raw_parts_mut(self.as_mut_ptr(), self.len()) }
+        }
     }
 }
