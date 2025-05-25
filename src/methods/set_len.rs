ed34e7d9e09f6c7ce710aa403eb26cecb23560e3
Prokop <prokop.schield@protonmail.com>
2025-05-25 14:10:54 +0200

    fix: Buffer::set_len

--- src/methods/set_len.rs
+++ src/methods/set_len.rs
@@ -6,11 +6,11 @@ impl Buffer {
     /// - `AllocationError` is returned if allocation fails.
     /// - `DeallocationError` is returned if deallocation fails.
     pub fn set_len(&mut self, new_len: usize) -> Result<&mut Self, BufferError> {
-        if new_len <= self.capacity() {
-            self.length = new_len;
-        } else {
+        if new_len > self.capacity() {
             self.realloc(new_len)?;
         }
+
+        self.length = new_len;
 
         Ok(self)
     }
